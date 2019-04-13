325ab3b0043d25b22abd9d39cd78a6e22ccff99d
todhm <todhm@nate.com>
2019-04-13 15:49:29 +0900

    change settings

--- airflow/dags/glovis_data_dags.py
+++ airflow/dags/glovis_data_dags.py
@@ -11,44 +11,53 @@ local_tz = pendulum.timezone("Asia/Seoul")
 thisYear = datetime.now().year 
 thisMonth = datetime.now().month -1 
 thisDate = datetime.now().day
-
-default_args = {
-    'owner': 'fidel',
-    'depends_on_past': False,
-    'start_date': datetime(thisYear, thisMonth, thisDate, hour=11,minute=30,tzinfo=local_tz),
-    'schedule_interval':'25 6 * * *',
-    'email': ['fidel@crunchprice.com'],
-    'email_on_failure': True,
-    'email_on_retry': True,
-    'retries': 5,
-    'retry_delay': timedelta(seconds=10),
-    'provide_context':True
-}
-
-dag_name = 'copy_goods_dags'
-dag = DAG(dag_name,schedule_interval=default_args['schedule_interval'],dagrun_timeout=timedelta(hours=10),default_args=default_args,concurrency=8, catchup=False)
-
-
-crawl_glovis_last_page = PythonOperator(
-    task_id='crawl_glovis_last_page',
-    python_callable=crawl_glovis_last_page,
-    dag=dag,
-    op_kwargs={
-        'loc':'sihwa' 
+loc_list = ['bundang','siwha','yangsan']
+def create_dag(loc):
+    default_args = {
+        'owner': 'fidel',
+        'depends_on_past': False,
+        'start_date': datetime(thisYear, thisMonth, thisDate, hour=11,minute=30,tzinfo=local_tz),
+        'schedule_interval':'0 11 * * *',
+        'email': ['fidel@crunchprice.com'],
+        'email_on_failure': True,
+        'email_on_retry': True,
+        'retries': 2,
+        'retry_delay': timedelta(seconds=10),
+        'provide_context':True
     }
-)
 
-crawl_glovis_pages = PythonOperator(
-    task_id='crawl_glovis_page',
-    python_callable=crawl_glovis_page,
-    dag=dag
-)
+    dag_name = 'crawl_auction_' +loc
+    if loc =="siwha":
+        dag = DAG(dag_name,schedule_interval=default_args['schedule_interval'],dagrun_timeout=timedelta(hours=1),default_args=default_args,concurrency=8, catchup=False)
+    else:
+        dag = DAG(dag_name,schedule_interval=None,dagrun_timeout=timedelta(hours=1),default_args=default_args,concurrency=8, catchup=False)
 
-upload_glovis_spread_task = PythonOperator(
-    task_id='upload_glovis_spread',
-    python_callable=upload_glovis_spread,
-    dag=dag
-)
 
-crawl_glovis_pages.set_upstream(crawl_glovis_last_page)
-upload_glovis_spread_task.set_upstream(crawl_glovis_pages)
+    crp = PythonOperator(
+        task_id='crawl_glovis_last_page',
+        python_callable=crawl_glovis_last_page,
+        dag=dag,
+        op_kwargs={
+            'loc':loc 
+        }
+    )
+
+    cgp = PythonOperator(
+        task_id='crawl_glovis_page',
+        python_callable=crawl_glovis_page,
+        dag=dag
+    )
+
+    upload_glovis_spread_task = PythonOperator(
+        task_id='upload_glovis_spread',
+        python_callable=upload_glovis_spread,
+        dag=dag
+    )
+
+    cgp.set_upstream(crp)
+    upload_glovis_spread_task.set_upstream(cgp)
+    return dag 
+
+for loc in loc_list:
+    dag_name = 'crawl_auction_' +loc
+    globals()[dag_name] = create_dag(loc)
